bca0b9ad4377edf59f4efdacf71c39bafd47fb4e
hacker-vaauwd <david.brooks.vaauwd@statefarm.com>
2020-07-15 22:27:01 -0500

    added frontEndJson

--- js/handler.js
+++ js/handler.js
@@ -22,8 +22,7 @@ for (let i = 0; i < 60; i++) {
 
 function getUser(uuid) {
   for (let i = 0; i < userTable.length; i++) {
-    if (uuid == userTable[i].uuid)
-      return userTable[i];
+    if (uuid == userTable[i].uuid) return userTable[i];
   }
   return null;
 }
@@ -33,6 +32,7 @@ console.log(`Mentors: ${mentorList.length} Mentees: ${menteeList.length}`);
 let privateUsers = "../private.json";
 let appointments = "../appointments.json";
 let connections = "../connections.json";
+let frontEndUsers = "../frontend.json";
 const fs = require("fs");
 
 let writeStream = fs.createWriteStream(privateUsers);
@@ -55,25 +55,48 @@ while (connectionsTable.length < amount) {
     }
   });
 }
-connectionsTable = [
-  ...new Set(connectionsTable)
-];
+connectionsTable = [...new Set(connectionsTable)];
 writeStream = fs.createWriteStream(connections);
 writeStream.write(JSON.stringify(connectionsTable), "utf8");
 
 // ======================Appointments=============================================================
 
-amount = api.randomNumber(
-  2,
-  connectionsTable.length
-);
+amount = api.randomNumber(2, connectionsTable.length);
 while (appointmentTable.length < amount) {
   connectionsTable.forEach((connection) => {
     let tor = getUser(connection.mentorUUID);
     let tee = getUser(connection.menteeUUID);
     appointmentTable.push(api.scheduleAppointment(tor, tee));
-  })
+  });
 }
 appointmentTable = [...new Set(appointmentTable)];
 writeStream = fs.createWriteStream(appointments);
 writeStream.write(JSON.stringify(appointmentTable), "utf8");
+
+// ======================FrontEndUsers=============================================================
+
+function getMentors(mentee) {
+  return connectionsTable.filter((c) => c.menteeUUID === mentee.uuid);
+}
+
+function getMentees(mentor) {
+  return connectionsTable.filter((c) => c.mentorUUID === mentor.uuid);
+}
+
+function getAppointments(user) {
+  return appointmentTable.filter((a) => {
+    a.host === user.username || a.guest === user.username;
+  });
+}
+
+amount = connections.length;
+let finalPrivate = [];
+userTable.forEach((person) => {
+  let p = JSON.parse(JSON.stringify(person))
+  p.mentors = p.isMentee ? getMentors(p) : [];
+  p.mentees = p.isMentor ? getMentees(p) : [];
+  p.agenda = getAppointments(p);
+  finalPrivate.push(p);
+});
+writeStream = fs.createWriteStream(frontEndUsers);
+writeStream.write(JSON.stringify(finalPrivate), "utf8");
